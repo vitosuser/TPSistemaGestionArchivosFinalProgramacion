0837ca344b04732d5f0156d95164ff66c01f07e6
vito <vitoromanloro@gmail.com>
2025-11-25 22:30:41 -0300

    estructura tp

---
+++ SolucionTPProgramacion/GestorDeArchivosDeTexto/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestorDeArchivosDeTexto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
